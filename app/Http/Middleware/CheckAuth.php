3cb63489d5f0fb8f62f213c16fa29b09baa8b9e8
zdx0823 <15916965182@163.com>
2021-04-28 16:21:15 +0800

    '+CheckAuth全局路由中间件，认证用户身份'

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+use App\Custom\CheckLogin\CheckLogin;
+use App\Custom\CheckSt\CheckSt;
+use App\Custom\Common\CustomCommon;
+
+class CheckAuth
+{
+
+    /**
+     * 是否登录，未登录是否有st
+     * 已登录返回true，st验证成功返回重定向链接
+     * 未登录，且st无效返回false
+     */
+    private static function checkLogin($request) {
+
+        if (CheckLogin::handle()) return true;
+
+        // 未登录，验证ST
+        if (CheckSt::handle($request)) {
+            // st存在且有效
+
+            // 删掉st
+            $uri = $request->getUri();
+
+            // 重定向链接
+            $redirectUrl = CustomCommon::delQuery($uri, ['st']);
+
+            // 下一步
+            return $redirectUrl;
+        }
+
+        return false;
+    }
+
+    
+    public function handle(Request $request, Closure $next)
+    {
+
+        // 是否登录
+        $checkLoginRes = self::checkLogin($request);
+
+        // 返回重定向链接
+        if (is_string($checkLoginRes)) {
+            return \redirect($checkLoginRes);
+        }
+
+        // 未登录，重定向到SSO
+        if ($checkLoginRes === false) {
+            $ssoLogin = config('custom.sso.login');
+            return \redirect()->away($ssoLogin);
+        }
+
+        // 已登录，是否有临时登录凭证
+        $loggedTokenKey = \config('custom.cookie.logged_tmp');
+
+        // 无凭证，返回邮箱验证码界面，二次验证
+        if (!cookie()->has($loggedTokenKey)) {
+            $authConfirm = config('custom.auth_confirm');
+            return \redirect($authConfirm);
+        }
+
+        // 有凭证，正常返回
+        return $next($request);
+    }
+}
